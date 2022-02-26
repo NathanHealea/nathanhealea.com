fc1329997d1b6d058645077078d75a757aeb0c48
Nathan Healea <nate.healea@gmail.com>
2022-02-25 23:41:17 -0800

    Feature: Implement FontAwesome

--- nathanhealea.web/gatsby-browser.js
+++ nathanhealea.web/gatsby-browser.js
@@ -15,5 +15,13 @@ import "@popperjs/core/dist/umd/popper.min.js";
 // Global Stylesheet
 import './src/styles/main.styles.scss'
 
+import { library} from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas, far, fab)
+
+
 export const wrapPageElement = PageWrapper
 export const wrapRootElement = RootWrapper
