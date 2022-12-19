f27dd099870f28ad205bb253f1bbc37795e4a924
Nathan Healea <nate.healea@gmail.com>
2022-12-19 11:09:12 -0800

    Update AppRoot and NextPage type definitions.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,30 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+import { FC } from 'react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+import { NextPage, AppProps } from '../next.types';
+
+import '../styles/globals.css';
+
+/**
+ * Determines if a page has a page layout.
+ * @param page next page that may or may not contain a page layout
+ * @returns true: page has a page layout, false: page does not have a page layout
+ */
+const hasPageLayout = (page: NextPage) =>
+  (page as NextPage).getLayout ? true : false;
+
+/**
+ * Application Root
+ */
+const App: FC<AppProps> = ({ Component, pageProps }) => {
+  /**
+   * Renders the layout of a page if layout is present
+   * @param page React Component exported from files in the page directories
+   * @returns Page React component wrapped around a layout if a layout for hte page is defined.
+   */
+  const renderPageLayout =
+    (hasPageLayout(Component) && Component.getLayout) || ((page: any) => page);
+
+  return renderPageLayout(<Component {...pageProps} />);
+};
+
+export default App;
