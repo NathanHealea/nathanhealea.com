f2f8c79fe0f2b4a067372c33e3d946536faf917c
Nathan Healea <nate.healea@gmail.com>
2023-05-26 22:08:00 -0700

    WIP - Reimplemented Google Analytics, Fix minor bugs, Added Twitter and OpenGraph meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { faCopyright } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import GoogleAnalytics from 'components/GoogleAnalytics';
 import { Logo } from 'components/Logo';
 import { Metadata } from 'next';
 import Link from 'next/link';
@@ -14,37 +15,50 @@ interface LayoutProps {
   children: ReactNode;
 }
 
-export const metadata: Metadata = {
-  title: 'Nathan Healea - Full Stack Developer & Software Engineer',
-  description:
-    'Experienced Full Stack Developer, engineering solutions in healthcare, fiance, and education. Passion for transforming ideas into feature rich applications.',
-    themeColor: '#f8f9fa',
-    // manifest: '',
-    // twitter: {
-    //   card: 'summary_large_image',
-    //   title: 'Next.js',
-    //   description: 'The React Framework for the Web',
-    //   siteId: '1467726470533754880',
-    //   creator: '@nextjs',
-    //   creatorId: '1467726470533754880',
-    //   images: ['https://nextjs.org/og.png'],
-    // },
-    viewport: {
-      width: 'device-width',
-      initialScale: 1,
-      maximumScale: 1,
-    },
-    other: {
-      'msapplicatoin-Title-Color': '#f8f9fa' 
-    }
-};
+const DEFAULT_TITLE =
+  'Nathan Healea - Full Stack Developer & Software Engineer';
+const DEFAULT_DESCRIPTION =
+  'Experienced Full Stack Developer, engineering solutions in healthcare, fiance, and education. Passion for transforming ideas into feature rich applications.';
+const DEFAULT_SITE_COLOR = '#f8f9fa';
 
+export const metadata: Metadata = {
+  title: DEFAULT_TITLE,
+  description: DEFAULT_DESCRIPTION,
+  themeColor: DEFAULT_SITE_COLOR,
+  // manifest: '',
+  twitter: {
+    card: 'summary_large_image',
+    title: DEFAULT_TITLE,
+    description: DEFAULT_DESCRIPTION,
+    site: '@nathanhealea',
+    siteId: '2654889620',
+    creator: '@nathanhealea',
+    creatorId: '2654889620',
+    images: [], // TODO: find photos for my twitter card,
+  },
+  openGraph: {
+    type: 'website',
+    title: DEFAULT_TITLE,
+    description: DEFAULT_DESCRIPTION,
+    url: 'https://nathanhealea.com',
+    images: [], // TODO: find images for my open Graph.
+  },
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+  },
+  other: {
+    'msapplicatoin-Title-Color': DEFAULT_SITE_COLOR,
+  },
+};
 
 const Layout: FC<LayoutProps> = (props) => {
   const { children } = props;
 
   return (
     <html lang='en'>
+      <GoogleAnalytics gaTrackingID={process.env.GOOGLE_ANALYTICS} />
       <body className='flex flex-col min-h-screen w-ful'>
         <header className='nav bg-white text-white-content sticky'>
           <div className='content flex flex-row flex-center h-32 py-0'>
--- src/hooks/useBreakpoints/useBreakpoints.enums.ts
+++
@@ -1,7 +0,0 @@
-export enum BreakpointEnum {
-  Sm = 'sm',
-  Md = 'md',
-  Lg = 'lg',
-  Xl = 'xl',
-  '2xl' = "2x'",
-}
