7501ef51d42c943e2c7ce57347971d7a5a6c6d01
Nathan Healea <nate.healea@gmail.com>
2023-05-29 09:48:21 -0700

    Updated LlamaUI

--- packages/llamaui/index.js
+++ packages/llamaui/index.js
@@ -1,20 +1,25 @@
 const plugin = require('tailwindcss/plugin');
-
-const colors = require('./src/colors');
 const addTypography = require('./src/typography');
 const addButtons = require('./src/buttons');
 const addLinks = require('./src/links');
+const addGradients = require('./src/gradients');
+const theme = require('./src/theme');
+
 const llmamauiPlugin = (props) => {
   console.log('🦙 LlamaUI');
   addTypography(props);
   addButtons(props);
   addLinks(props);
+  addGradients(props);
 };
 
 module.exports = plugin(llmamauiPlugin, {
   theme: {
+    colors: theme.colors,
     extend: {
-      colors,
+      spacing: {
+        '1/7': '14.286%',
+      },
     },
   },
 });
--- packages/llamaui/src/colors/index.js
+++ packages/llamaui/src/colors/index.js
@@ -1,33 +1,3 @@
-const generateColor = require('./createColor');
+const addColors = ({}) => {};
 
-const white = {
-  DEFAULT: '#F8F9FA',
-  content: '#212529',
-};
-
-const black = {
-  DEFAULT: '#212529',
-  content: '#F8F9FA',
-};
-
-// const strawberry = generateColor('#FFADAD');
-// const apricot = generateColor('#FFD6A5');
-// const lemon = generateColor('#FDFFB6');
-// const apple = generateColor('#CAFFBF');
-// const blueberry = generateColor('#9BF6FF');
-// const plum = generateColor('#A0C4FF');
-// const grape = generateColor('#BDB2FF');
-// const peach = generateColor('#FFC6FF');
-
-module.exports = {
-  white,
-  black,
-  // strawberry,
-  // apricot,
-  // lemon,
-  // apple,
-  // blueberry,
-  // plum,
-  // grape,
-  // peach,
-};
+module.exports = addColors;
