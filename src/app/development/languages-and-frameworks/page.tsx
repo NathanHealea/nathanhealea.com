96dc973c6e20e27320f305ef4d1cfaebe1b9bf99
Nathan Healea <nate.healea@gmail.com>
2023-06-08 15:28:27 -0700

    New Languages and Frameworks list page

---
+++ src/app/development/languages-and-frameworks/page.tsx
@@ -0,0 +1,37 @@
+import { Metadata, NextPage } from 'next';
+import Link from 'next/link';
+import { FC } from 'react';
+
+interface LanguagesAndFrameworksPageProps {}
+
+const PAGE_TITLE = 'Language and Framework List';
+const PAGE_DESCRIPTION =
+  'My personal language and framework ranking list, complete with pros, cons, and thoughts. ';
+
+export const metadata: Metadata = {
+  title: `Nathan Healea - ${PAGE_TITLE}`,
+  description: PAGE_DESCRIPTION,
+};
+
+const LanguagesAndFrameworksPage = (props: LanguagesAndFrameworksPageProps) => {
+  return (
+    <>
+      <main className='flex flex-col flex-1 justify-center items-center'>
+        <div className=' max-w-7xl mx-auto flex flex-col text-center gap-y-4'>
+          <h1>✨ Coming Soon ✨</h1>
+          <h3 className='text-overline'>{PAGE_TITLE}.</h3>
+          <p>{PAGE_DESCRIPTION}</p>
+
+          <p>Lets try one of these links ⛛</p>
+          <div className='flex flex-col justify-center items-center gap-x-4'>
+            <Link href='/' className='btn btn-outlined btn-strawberry'>
+              Home
+            </Link>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default LanguagesAndFrameworksPage;
