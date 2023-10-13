a185493a42c3251f2ba05e16cd7cf764d9acf3c7
Nathan Healea <nhealea@uoregon.edu>
2023-10-13 10:06:22 -0700

    WIP - SAVING WORK IN PROGRESS

--- src/components/SimpleIcon/SimpleIcon.component.tsx
+++ src/components/SimpleIcon/SimpleIcon.component.tsx
@@ -1,39 +1,52 @@
 'use client';
 
-import theme from '../../../packages/llamaui/src/theme';
+// import theme from '../../../packages/llamaui/src/theme';
 import { Color } from './SimpleIcon.types';
 import Image from 'next/image';
 import { FC } from 'react';
 
 type SafeNumber = number | `${number}`;
 
-const colors: { [key: string]: string } = Object.keys(theme.colors).reduce(
-  (colorsDic, currentColor: string) => {
-    const color = theme.colors[currentColor] as { [key: string]: string };
-    return {
-      ...colorsDic,
-      ...Object.keys(color).reduce(
-        (colorValueDic, currentColorValue: string) => {
-          const value = color[currentColorValue];
+// const colors: { [key: string]: string } = Object.keys(theme.colors).reduce(
+//   (colorsDic, currentColor: string) => {
+//     const color = theme.colors[currentColor] as { [key: string]: string };
+//     return {
+//       ...colorsDic,
+//       ...Object.keys(color).reduce(
+//         (colorValueDic, currentColorValue: string) => {
+//           const value = color[currentColorValue];
 
-          if (currentColorValue === 'DEFAULT') {
-            return {
-              ...colorValueDic,
-              [`${currentColor}`]: value.replace('#', ''),
-            };
-          }
+//           if (currentColorValue === 'DEFAULT') {
+//             return {
+//               ...colorValueDic,
+//               [`${currentColor}`]: value.replace('#', ''),
+//             };
+//           }
 
-          return {
-            ...colorValueDic,
-            [`${currentColor}-${currentColorValue}`]: value.replace('#', ''),
-          };
-        },
-        {}
-      ),
-    };
-  },
-  {}
-);
+//           return {
+//             ...colorValueDic,
+//             [`${currentColor}-${currentColorValue}`]: value.replace('#', ''),
+//           };
+//         },
+//         {}
+//       ),
+//     };
+//   },
+//   {}
+// );
+
+const colors: { [key: string]: string } = {
+  white: '',
+  black: '',
+  strawberry: '',
+  apricot: '',
+  lemon: '',
+  apple: '',
+  blueberry: '',
+  plum: '',
+  grape: '',
+  peach: '',
+};
 
 interface SimpleIconProps {
   name: string;
@@ -46,12 +59,12 @@ interface SimpleIconProps {
 const SimpleIcon: FC<SimpleIconProps> = (props) => {
   const { name, slug, color = 'white', width = 32, height = 32 } = props;
 
-  console.log(colors);
+  // console.log(colors);
 
   const getSimpleIconUrl = () => {
     const baseURL = 'https://cdn.simpleicons.org';
 
-    console.log([baseURL, slug, colors['white']].join('/'));
+    console.log([baseURL, slug, colors[color]].join('/'));
 
     return [baseURL, slug, colors[color]].join('/');
   };
