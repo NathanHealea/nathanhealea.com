21d777af25923e0bf585f46a2eda793d540f013b
Nathan Healea <nate.healea@gmail.com>
2024-03-27 22:39:48 -0700

    Add package headlessui; Add Headlessui to Tailwind configuration;

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -44,7 +44,7 @@ export default {
 
     extend: {},
   },
-  plugins: [require('daisyui')],
+  plugins: [require('daisyui'), require('@headlessui/tailwindcss')],
 
   daisyui: {
     themes: ['light'],
