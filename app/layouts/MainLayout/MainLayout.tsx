53cc70447d7c6f558487a9cf7b068aeb16b9c2c0
Nathan Healea <nhealea@uoregon.edu>
2024-07-01 11:37:37 -0700

    Fix header/navigation glass effect

--- app/layouts/MainLayout/MainLayout.tsx
+++ app/layouts/MainLayout/MainLayout.tsx
@@ -1,9 +1,5 @@
 import { ReactNode } from 'react';
 import Header from './components/Header';
-import Navigation from './components/Navigation';
-import { routes, Route } from '~/routes.config';
-import { Link } from 'react-router-dom';
-import { v4 as uuid } from 'uuid';
 
 interface MainLayoutProps {
   children: ReactNode;
@@ -13,18 +9,8 @@ const MainLayout = (props: MainLayoutProps) => {
   const { children } = props;
   return (
     <>
-      <Header>
-        <Navigation>
-          {routes.map((route: Route) => {
-            return (
-              <Link key={uuid()} to={route.getPath()}>
-                {route.title}
-              </Link>
-            );
-          })}
-        </Navigation>
-      </Header>
-      <div className='h-24 ' />
+      <Header />
+      <div className="h-24 " />
       {children}
     </>
   );
--- app/layouts/MainLayout/components/Navigation.tsx
+++ app/layouts/MainLayout/components/Navigation.tsx
@@ -1,7 +1,6 @@
 import { Menu, Transition } from '@headlessui/react';
 import { Bars3Icon } from '@heroicons/react/24/solid';
 import { Link } from '@remix-run/react';
-import clsx from 'clsx';
 import { Children, ComponentProps, cloneElement } from 'react';
 import { v4 as uuid } from 'uuid';
 export interface NavigationProps extends ComponentProps<'nav'> {}
@@ -11,40 +10,43 @@ const Navigation = (props: NavigationProps) => {
 
   return (
     <>
-      <Menu as='nav' className='relative inset-0 z-10 flex w-full justify-center before:backdrop-blur'>
-        <div className={clsx('navbar w-full  min-w-80 max-w-7xl rounded-2xl shadow-xl backdrop-blur ', className)}>
-          <div className='flex'>
-            <Link to='/' className='btn btn-ghost '>
-              Nathan Healea
-            </Link>
-          </div>
+      <Menu as="nav" className="navbar relative w-full min-w-80 max-w-7xl ">
+        <div className="flex">
+          <Link to="/" className="btn btn-ghost ">
+            Nathan Healea
+          </Link>
+        </div>
 
-          <div className='mobile-menu flex-1 items-end justify-end md:hidden'>
-            <Menu.Button className='btn btn-ghost'>
-              <Bars3Icon className='h-8 w-8' />
-            </Menu.Button>
-          </div>
-          <div className='hidden flex-1 justify-end md:flex'>
-            <ul className='menu menu-horizontal'>
-              {Children.map(children, (child) => {
-                return <li key={uuid()}>{cloneElement(child, { className: 'p-4 px-8' })}</li>;
-              })}
-            </ul>
-          </div>
+        <div className="mobile-menu flex-1 items-end justify-end md:hidden">
+          <Menu.Button className="btn btn-ghost">
+            <Bars3Icon className="h-8 w-8" />
+          </Menu.Button>
+        </div>
+        <div className="hidden flex-1 justify-end md:flex">
+          <ul className="menu menu-horizontal">
+            {Children.map(children, (child) => {
+              return (
+                <li key={uuid()}>
+                  {cloneElement(child, { className: 'p-4 px-8' })}
+                </li>
+              );
+            })}
+          </ul>
         </div>
 
         <Transition
-          className='absolute right-0 top-24 w-full max-w-7xl rounded-2xl shadow-xl backdrop-blur sm:max-w-xl md:hidden'
-          enter='transition duration-100 ease-out'
-          enterFrom='transform scale-95 opacity-0'
-          enterTo='transform scale-100 opacity-100'
-          leave='transition duration-75 ease-out'
-          leaveFrom='transform scale-100 opacity-100'
-          leaveTo='transform scale-95 opacity-0'>
-          <Menu.Items as='ul' className='menu'>
+          className=" bg-opacity-700 bg-base-700 [transform:translate3d(0,0,0)]md:hidden navbar sticky  right-0 top-24 z-30  flex h-16 w-full justify-center rounded-2xl text-base-content shadow-xl backdrop-blur transition-shadow duration-100"
+          enter="transition duration-100 ease-out"
+          enterFrom="transform scale-95 opacity-0"
+          enterTo="transform scale-100 opacity-100"
+          leave="transition duration-75 ease-out"
+          leaveFrom="transform scale-100 opacity-100"
+          leaveTo="transform scale-95 opacity-0"
+        >
+          <Menu.Items as="ul" className="menu">
             {Children.map(children, (child) => {
               return (
-                <Menu.Item as='li' key={uuid()}>
+                <Menu.Item as="li" key={uuid()}>
                   {cloneElement(child, { className: 'p-4 px-8' })}
                 </Menu.Item>
               );
